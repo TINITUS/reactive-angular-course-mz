764eb1b18480322a8fd4a805bc60e2d1ba64ae59
Miroslav Zografski <mzografski@soft2run.com>
2023-04-15 00:31:47 +0300

    Test

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -13,11 +13,18 @@ import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from "moment";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
 
 @Component({
   selector: "course-dialog",
   templateUrl: "./course-dialog.component.html",
   styleUrls: ["./course-dialog.component.css"],
+  providers: [
+    LoadingService,
+    MessagesService
+  ],
 })
 export class CourseDialogComponent implements AfterViewInit {
   form: FormGroup;
@@ -27,7 +34,10 @@ export class CourseDialogComponent implements AfterViewInit {
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) course: Course
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private coursesService: CoursesService,
+    private loadingService: LoadingService,
+    private messagesService: MessagesService
   ) {
     this.course = course;
 
@@ -43,6 +53,19 @@ export class CourseDialogComponent implements AfterViewInit {
 
   save() {
     const changes = this.form.value;
+    const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+      .pipe(
+        catchError(err => {
+          const message = "Could not save course";
+          this.messagesService.showErrors(message);
+          console.log(message, err);
+          return throwError(err);
+        })
+      );
+    this.loadingService.showLoaderUntilCompleted(saveCourse$)
+      .subscribe(
+        (val) => this.dialogRef.close(val)
+      );
   }
 
   close() {
--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -4,27 +4,25 @@ import { concatMap, finalize, tap } from "rxjs/operators";
 
 @Injectable()
 export class LoadingService {
+
   private loadingSubject = new BehaviorSubject<boolean>(false);
 
   loading$: Observable<boolean> = this.loadingSubject.asObservable();
-
   constructor() {
-    console.log("Loading service created ...");
+    console.info("LoadingService created");
   }
-
   showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
-    return of(null).pipe(
-      tap(() => this.loadingOn()),
-      concatMap(() => obs$),
-      finalize(() => this.loadingOff())
-    );
+    return of(null)
+      .pipe(
+        tap(() => this.loadingOn()),
+        concatMap(() => obs$),
+        finalize(() => this.loadingOff())
+      )
   }
-
-  loadingOn() {
+  private loadingOn() {
     this.loadingSubject.next(true);
   }
-
-  loadingOff() {
+  private loadingOff() {
     this.loadingSubject.next(false);
   }
 }
